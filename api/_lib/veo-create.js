63cb46e5131cc8ac9bb40d82777d92942b2da3e8
OMRAN77 <ommntr77@gmail.com>
2026-07-25 14:50:32 +0400

    dep31: remove personGeneration (Veo rejects allow_adult)

--- api/_lib/veo-create.js
+++ api/_lib/veo-create.js
@@ -36,7 +36,7 @@ module.exports = async (req, res) => {
       headers: { 'x-goog-api-key': apiKey, 'Content-Type': 'application/json' },
       body: JSON.stringify({
         instances: [{ prompt }],
-        parameters: { aspectRatio, personGeneration: 'allow_adult' },
+        parameters: { aspectRatio },
       }),
     });
     const data = await upstream.json().catch(() => ({}));
